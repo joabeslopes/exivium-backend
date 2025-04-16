e2ea48412c236c679041f9df359695f4b8c570d7
joabeslopes <joabeslopes.jsl@gmail.com>
2025-04-16 18:51:13 -0300

    organizaçao do codigo e qualidade jpeg

--- app.py
+++ app.py
@@ -3,44 +3,62 @@ import asyncio
 import threading
 import uvicorn
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import atexit
 
 app = FastAPI()
 
-JPEG_QUALITY = 60
+JPEG_QUALITY = 70
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 FPS = 30
-
-valid_tokens = {
-                "meu_token": "usuario1", 
-                "meu_token_plugin2": "plugin2",
-                }
-
-camera_channels = {
-    1: "rtsp://localhost:8554/mystream"
-}
-
 active_cameras = {}
+
+def log(mensagem: str):
+    print(mensagem)
+
+def get_channel(channel: int=None):
+    all_channels = {
+        1: "rtsp://localhost:8554/mystream"
+    }
+    if channel is None:
+        return all_channels
+    else:
+        return all_channels[channel]
+
+def validate_token(token: str):
+    valid_tokens = {
+                    "meu_token": "usuario1", 
+                    "meu_token_plugin2": "plugin2",
+                    }
+    return token in valid_tokens
+
 
 class Camera:
     def __init__(self, channel: int):
         self.frame = None
         self.running = False
+        self.channel = channel
 
-        self.url = camera_channels[channel]
-        if not self.url:
-            print("[ERRO CAMERA] Canal inválido.")
+        self.url = get_channel(channel)
+        if self.url is None:
+            log(f"[ERRO] Canal {channel} nao existe")
             return
 
         self.cap = cv2.VideoCapture(self.url)
+
+        self.running = self.cap.isOpened()
+        if not self.running:
+            log(f"[ERRO] Camera {self.url} invalida")
+            return
+
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
         self.cap.set(cv2.CAP_PROP_FPS, FPS)
         self.thread = threading.Thread(target=self._capture, daemon=True)
         self.thread.start()
+        atexit.register(self.stop)
 
     def _capture(self):
-        self.running = True
         while self.running:
             ret, frame = self.cap.read()
             if ret:
@@ -51,27 +69,35 @@ class Camera:
         return self.frame
 
     def stop(self):
+        log(f"Parando camera {self.channel}")
         self.running = False
         self.cap.release()
 
 
-for channel in camera_channels:
-    active_cameras[channel] = Camera(channel)
+def run_cameras():
+    log('Ligando cameras')
+    camera_channels = get_channel()
+    for channel in camera_channels:
+        active_cameras[channel] = Camera(channel)
+
 
 @app.websocket("/ws/video/{channel}")
 async def video_stream(websocket: WebSocket, token: str, channel: int):
-    if token not in valid_tokens:
+    if not validate_token(token):
+        log("[ERRO] Token invalido")
         raise WebSocketDisconnect(code=1008, reason="Token inválido")
 
     if channel not in active_cameras:
+        log(f"[ERRO] Camera {channel} nao encontrada")
         raise WebSocketDisconnect(code=1008, reason="Camera nao encontrada")
 
     await websocket.accept()
-    print("cliente conectado")
+    log("Cliente conectado")
 
     camera = active_cameras[channel]
 
     if not camera.running:
+        log(f"Camera {channel} nao está funcionando")
         raise WebSocketDisconnect(code=1008, reason="Camera nao está funcionando")
 
     try:
@@ -81,21 +107,24 @@ async def video_stream(websocket: WebSocket, token: str, channel: int):
                 await websocket.send_bytes(frame)
             await asyncio.sleep(1 / FPS)
     except WebSocketDisconnect:
-        print("cliente desconectado")
+        log("Cliente desconectado")
 
 @app.websocket("/ws/results")
 async def result_receiver(websocket: WebSocket, token: str):
-    if token not in valid_tokens:
+    if not validate_token(token):
+        log("[ERRO] Token invalido")
         raise WebSocketDisconnect(code=1008, reason="Token inválido")
 
     await websocket.accept()
     try:
         while True:
             data = await websocket.receive_text()
-            print(f"Cliente enviou: {data}")
+            log(f"Cliente enviou: {data}")
     except WebSocketDisconnect:
-        print("Cliente desconectado")
+        log("Cliente desconectado")
 
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000)
+elif __name__ == "app":
+    run_cameras()
